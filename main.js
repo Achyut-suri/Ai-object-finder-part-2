15c5b1cf20b0d834236573926f45048e2aa53326
Achyut-suri <88927531+Achyut-suri@users.noreply.github.com>
2021-12-19 15:51:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+status = " ";
+
+input_box = " ";
+
+
+
+function setup()
+{
+canvas = createCanvas(480 , 380);
+canvas.center();
+
+video = createCapture(VIDEO);
+video.size(480 , 380);
+video.hide();
+}
+
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd' , modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects.";
+    input_box = document.getElementById("input_box").value;
+}
+
+function modelLoaded()
+{
+console.log("Model Loaded!");
+status = true;
+
+}
+
+function draw()
+{
+image(video , 0 , 0 , 480 , 380);
+}
+
+
+
